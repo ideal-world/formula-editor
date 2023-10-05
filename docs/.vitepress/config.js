1cf18dfdc520ff028d5f4ebacc7f463edf674ffd
gudaoxuri <i@sunisle.org>
2023-10-05 19:45:04 +0800

    Add api documentation.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,14 @@
 export default {
-    title: 'Formula Editor',
-    description: 'A relatively complete formula editor',
-    base: '/formula-editor/',
-    themeConfig: {
-        nav: [
-            {text: 'Introduction', link: '/'},
-            {text: 'Edit Demo', link: '/editor-demo'},
-            {text: 'Execute Demo', link: '/executor-demo'}
-        ],
-        socialLinks: [
-            {icon: 'github', link: 'https://github.com/ideal-world/formula-editor'}
-        ]
-    }
+  title: 'Formula Editor',
+  description: 'A relatively complete formula editor',
+  base: '/formula-editor/',
+  themeConfig: {
+    nav: [
+      { text: 'Introduction', link: '/' },
+      { text: 'Edit Demo', link: '/editor-demo' },
+      { text: 'Execute Demo', link: '/executor-demo' },
+      { text: 'API', link: '/api/exports' },
+    ],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/ideal-world/formula-editor' }],
+  },
 }
