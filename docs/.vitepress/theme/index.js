0a188855c1601f72c588f7acb7cfb2db8ec009c8
gudaoxuri <i@sunisle.org>
2023-10-09 01:01:04 +0800

    Fixed ReferenceError: __VUE_PROD_DEVTOOLS__ is not defined

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,11 +4,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import FormulaEditor from '../../../src'
 
+globalThis.__VUE_PROD_DEVTOOLS__ = false
+
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.use(ElementPlus)
     app.use(FormulaEditor)
     app.component('DemoContainer', DemoContainer)
-  }
+  },
 }
